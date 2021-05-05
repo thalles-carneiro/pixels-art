09de4da2c7b01b92ff9fcf96d62d598b66bb205f
Thalles Carneiro <thallescarneiro@gmail.com>
2021-05-05 15:31:27 -0300

    Requisito 4

---
+++ script.js
@@ -0,0 +1,29 @@
+window.onload = function () {
+  let boardSide = 5;
+  let getBoard = document.getElementById('pixel-board');  
+
+  //Chama as funções
+  createTable();
+
+  //Cria dinamicamente uma linha da tabela com um determinado número de colunas
+  function createTableRow(row) {
+    let tableRow = document.createElement('tr');
+    tableRow.className = 'board-row';
+    getBoard.appendChild(tableRow);
+    let getBoardRow = document.getElementsByClassName('board-row')[row];
+    for (let column = 1; column <= boardSide; column += 1) {
+      const tableColumn = document.createElement('td');
+      tableColumn.className = 'pixel';
+      tableColumn.style.backgroundColor = 'white';
+      getBoardRow.appendChild(tableColumn);
+    }
+  }
+
+  //Cria dinamicamente um determinado número de linhas da tabela
+  function createTable() {
+    for (let row = 0; row < boardSide; row += 1) {
+      createTableRow(row);
+    }
+  }
+
+}

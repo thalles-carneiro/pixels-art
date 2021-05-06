29cc01b4500801ba4d343dc6412a33f07951e2b3
Thalles Carneiro <thallescarneiro@gmail.com>
2021-05-05 23:10:11 -0300

    Requisito 10 - Resolvendo o início do requisito

--- script.js
+++ script.js
@@ -1,34 +1,36 @@
 window.onload = function () {
-  let boardSide = 5;
+  let boardSide = document.getElementById('board-size').value;
   let getBoard = document.getElementById('pixel-board');  
   let firstColor = document.querySelector('.color1');
-    
-  //Inserir um escutador de eventos em cada elemento do documento
+  firstColor.classList.add('selected');
+  
+  createTable(boardSide);
+
   document.addEventListener('click', function (event) {
-    //Clicar em uma das cores da paleta faz com que ela seja selecionada
     if (event.target.classList.contains('color')) {
       setSelected(event);
     };
-        
-    //Clicar em um pixel dentro do quadro após selecionar uma cor na paleta faz com que o pixel seja preenchido com a cor selecionada
     if (event.target.classList.contains('pixel')) {
       setPixelBackgroundColor(event);
     };
-
-    //Clicar no botão de Limpar preenche todos os pixels do quadro de branco
     if (event.target.id === 'clear-board') {
       resetBoard();
-    }
-
+    };
+    if (event.target.id === 'generate-board') {
+      boardSide = document.getElementById('board-size').value;
+      if (boardSide > 0) {
+        getBoard.innerHTML = '';
+        createTable(boardSide);
+      }
+    };
   });
   
-  //Define a cor preta como cor inicial
-  firstColor.classList.add('selected');
-  
-  //Chama as funções
-  createTable();
-  
-  //Cria dinamicamente uma linha da tabela com um determinado número de colunas
+  function createTable(boardSide) {
+    for (let row = 0; row < boardSide; row += 1) {
+      createTableRow(row);
+    };
+  };
+
   function createTableRow(row) {
     let tableRow = document.createElement('tr');
     tableRow.className = 'board-row';
@@ -41,22 +43,13 @@ window.onload = function () {
       getBoardRow.appendChild(tableColumn);
     };
   };
-  
-  //Cria dinamicamente um determinado número de linhas da tabela
-  function createTable() {
-    for (let row = 0; row < boardSide; row += 1) {
-      createTableRow(row);
-    };
-  };
 
-  //Cria uma função que adiciona a classe 'selected' ao elemento selecionado, mas pode existir apenas um elemento com essa classe
   function setSelected(event) {
     const selectedElement = document.querySelector('.selected');
     selectedElement.classList.remove('selected');
     event.target.classList.add('selected');
   };
 
-  //Cria uma função que modifica o backgroundColor do pixel clicado de acordo com a cor da paleta selecionada
   function setPixelBackgroundColor(event) {
     const selectedElement = document.querySelector('.selected');
     const selectedBackgroundColor = window.getComputedStyle(selectedElement).getPropertyValue('background-color');
@@ -68,13 +61,11 @@ window.onload = function () {
     };
   };
 
-  //Cria um função que reseta o backgroundColor de todos os pixels para branco
   function resetBoard() {
     const allPixels = document.getElementsByClassName('pixel');
     for (let pixel of allPixels) {
       pixel.style.backgroundColor = 'white';
     };
   };
-   
-};
 
+};
